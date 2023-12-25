3d486e5582a0dd1a63d840bcb1455216d1330417
Ruslan <ruslan.g@agilesolutions.az>
2023-12-25 11:45:50 +0400

    first

---
+++ Lesson 1/Program.cs
@@ -0,0 +1,33 @@
+﻿//See https://aka.ms/new-console-template for more information
+
+
+while(true)
+{
+	try
+	{
+        Console.WriteLine("bir eded daxil et:");
+		int number = int.Parse(Console.ReadLine());
+		//int number = int.Parse(Console.ReadLine());
+		//long number = Convert.ToInt64(Console.ReadLine());
+
+		//int number1 = "dsadasda";
+		//int number2 = 2;
+		
+	    
+
+  //      string data = "" + number1 + number2;
+		//Console.WriteLine(data.GetType());
+		//Console.WriteLine(number.GetType());
+		//string num = number.ToString();
+  //      Console.WriteLine(num.GetType());
+
+        //Console.WriteLine(number);
+		break;
+    }
+	catch (Exception ex)
+	{
+		continue;
+	}
+}
+
+Console.ReadLine();
